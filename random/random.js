9adfddf012efc5696136cf5e3a265ee7819a132e
Mwni <manuel@tigmar.de>
2023-07-13 05:13:25 +0200

    add random token generation function

---
+++ random/random.js
@@ -0,0 +1,22 @@
+import { randomBytes } from 'crypto'
+
+
+export function generateRandomToken({ 
+	segments = 1, 
+	charactersPerSegment = 6, 
+	alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' 
+}){
+	return Array(segments)
+		.fill(0)
+		.map(() => {
+			let bytes = randomBytes(charactersPerSegment)
+			let str = ''
+
+			for(let i=0; i<bytes.length; i++){
+				str += alphabet[bytes[i] % alphabet.length]
+			}
+
+			return str
+		})
+		.join('-')
+}
